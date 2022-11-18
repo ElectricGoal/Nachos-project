80e59bb3c7e8092f49873e15c7e06aad5ed14651
tkiet12 <46044589+tkiet12@users.noreply.github.com>
2022-11-18 14:58:13 +0700

    add read, write

---
+++ NachOS-4.0/code/test/ReadWrite.c
@@ -0,0 +1,25 @@
+#include "syscall.h"
+
+int main(){
+    int fd;
+    int len;
+    char buffer[100];
+
+    PrintString("Open read write testReadWrite.txt\n");
+    fd = Open("testReadWrite.txt", 0);
+
+    // test read write console
+    PrintString("Enter a text to write to opened file: ");
+    len = Read(buffer, 10, 0);
+    PrintString("The entered text is: ");
+    Write(buffer, len, 1);
+
+    // write the entered text to file
+    Write(buffer, len, fd);
+
+    Close(fd);
+    PrintString("\nClosed testReadWrite.txt\n");
+
+    Halt();
+    return 0;
+}
