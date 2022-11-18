9a1b343668b8cbd97c862cd52bdbc5c0f3c71599
trantinvan21087 <74363485+trantinvan21087@users.noreply.github.com>
2022-11-18 23:47:01 +0700

    update concatenate

--- NachOS-4.0/code/test/concatenate.c
+++ NachOS-4.0/code/test/concatenate.c
@@ -1,6 +1,13 @@
 #include "syscall.h"
 #define MAX_LENGTH 255
 
+#define MODE_READWRITE 0
+#define MODE_READ 1
+
+#define stdin 0
+#define stdout 1
+
+
 char *strcat(char *s1, const char *s2) {
   char *s = s1;
 
@@ -13,10 +20,18 @@ char *strcat(char *s1, const char *s2) {
 }
 
 int main() {
+    int i;
+    int write;
+    char buffer[100];
+    //int fileid;
+    int read1;
+    int read2;
+    int len1 = 0;
+    int len2 = 0;
     char fileName1[MAX_LENGTH];
     char fileName2[MAX_LENGTH];
-    OpenFileID fileId1;
-    OpenFileID fileId2;
+    int fileId1;
+    int fileId2;
     // int length;
 
     // PrintString("Enter file's name's length: ");
@@ -26,7 +41,39 @@ int main() {
 
     PrintString("Enter second file's name: ");
     ReadString(fileName2, MAX_LENGTH);
+    
+    PrintString("Open first file \n");
+    fileId1 = Open(fileName1, MODE_READ);
+    read1 = Read(buffer, 50, fileId1);
+    while (buffer[len1] != '\0') ++len1;
+    PrintString("Read ");
+    PrintNum(len1);
+    PrintString(" characters: ");
+    PrintString(buffer);
+    PrintString("\n");
+    Close(fileId1);
 
+    PrintString("Open second file \n");
+    fileId2 = Open(fileName2, MODE_READWRITE);
+    read2 = Read(buffer, 50, stdin);
+    len2 = 0;
+    while (buffer[len2] != '\0') ++len2;
+    Write(buffer, len2, stdout);
+    PrintString("\n");
+
+
+    write = Write(buffer, len2, fileId2);
+
+    PrintString("Write ");
+    PrintNum(write);
+    PrintString(" characters: ");
+    PrintString(buffer);
+    PrintString("\n");
+    Close(fileId2);
+
+    PrintString("Enter a string:\n");
+        PrintString("You have entered: ");
+    
 
     Halt();
 }
